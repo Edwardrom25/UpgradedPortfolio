b2da0e5b0007450e532c4c9e7e536dd5bf4cef81
Edwardrom25 <edwardromero781@gmail.com>
2025-01-01 10:51:33 -0600

    Description of changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -208,7 +208,7 @@ import {
       points: [
         "Supported the new product development (NPD) and quality teams in the manufacturing and testing of metal and plastic components for cleaning products used in ISO 5–ISO 8 cleanrooms in the aerospace industry.",
         "Led a failure analysis of a wheel cleaner prototype by programming a Universal Robots UR5 Cobot Arm to simulate a trolley passing through 1,000+ times, implementing corrective actions that improved component reliability.",
-        "Coordinated third-party particle release testing and led autoclavability and chemical resistance testing in the R&D lab on prototypes made from recycled materials for a new eco-friendly product line, compiling the data into PowerPoint reports.",
+        "Coordinated third-party particle release testing and led autoclavability and chemical resistance testing in the R&D lab on prototypes made from recycled plastics for a new eco-friendly product line, compiling the data into PowerPoint reports.",
         "Created bills of materials, quality control plans, and packaging process specifications for 6 products, utilizing TOPS Pro to optimize pallet patterns for improved cost savings.",
         "Developed 2D drawings in SolidWorks for 9 parts of a flagship product launching in 2025, incorporating geometric dimensioning and tolerancing (GD&T) in connection areas to minimize assembly issues for end users.",
         "Designed a test method to compute the coefficient of friction of microfiber cleaning materials, providing the sales team with a data-driven metric to highlight the ergonomic advantages of Vileda Professional products over competitors.",
