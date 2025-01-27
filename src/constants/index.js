5da57237d7c30a33447ca9814cac56cc22a7854c
Edwardrom25 <edwardromero781@gmail.com>
2025-01-26 18:48:52 -0600

    Describe your changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -209,7 +209,7 @@ import {
       date: "June 2024 - August 2024",
       points: [
         "Supported the new product development (NPD) and quality teams in designing, testing, and manufacturing metal and plastic components for microfiber cleaning systems used for contamination control in cleanrooms.",
-        "Led gamma irradiation, particle release, cleaning efficacy, autoclavability, and chemical resistance testing on prototypes made from recycled materials to ensure a new sustainability-focused product line met reliability goals.",
+        "Led gamma irradiation, particle release, cleaning efficacy, autoclavability, and chemical resistance testing on prototypes made from recycled materials to ensure a new eco-friendly product line met reliability goals.",
         "Designed a test method to compute the coefficient of friction of microfiber cleaning materials, providing the sales team with a data-driven metric to highlight the ergonomic advantages of Vileda Professional products over competitors.",
         "Investigated and resolved quality complaints by determining root causes and implementing corrective actions.",
         "Created product specifications, bills of materials, quality control plans, and packaging process specifications for new and existing products.",
