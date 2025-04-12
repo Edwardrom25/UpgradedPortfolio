a68a760589c88913f8d63da9e9f141f5ccb2d2a8
Edwardrom25 <edwardromero781@gmail.com>
2025-04-12 13:00:25 -0500

    Updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -228,7 +228,7 @@ import {
     {
       name: "NASA L'SPACE Proposal Writing and Evaluation Experience",
       description:
-        "Co-authored a proposal on flight computing and avionics technologies to secure a $10K seed fund from NASA Marshall Space Flight Center’s Chief Technologist.",
+        "Co-authored a proposal on a flight computing and avionics technology to secure a $10K seed fund from NASA Marshall Space Flight Center’s Chief Technologist.",
       tags: [
         {
           name: "Siemens NX",
