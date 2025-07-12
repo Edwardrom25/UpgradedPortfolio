e3ff0ee6d2aa36c2da95ff3600f62d7429270a97
Edwardrom25 <edwardromero781@gmail.com>
2025-07-12 16:20:08 -0500

    wording

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px] text-left'
       >
-        Pursuing a master’s degree in electrical and computer engineering at the University of Illinois Urbana-Champaign. Passed the Fundamentals of Engineering Electrical and Computer exam, earning my Engineer-in-Training certification. Helping make life multiplanetary!
+        Pursuing a master’s degree in electrical and computer engineering at the University of Illinois Urbana-Champaign. Passed the Fundamentals of Engineering Electrical and Computer exam. Making life multiplanetary!
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10 justify-center'>
