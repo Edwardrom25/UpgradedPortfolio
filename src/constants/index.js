2db8800d2f068e4b38d72c744da7e3630811157f
Edwardrom25 <edwardromero781@gmail.com>
2025-07-20 09:21:26 -0500

    Biological systems

--- src/constants/index.js
+++ src/constants/index.js
@@ -162,7 +162,7 @@ import {
       iconBg: "#FFFFFF",
       date: "January 2025 - May 2025",
       points: [
-        "Supported NASA’s Space Biology program in examining the impacts of spaceflight on living systems to enable a permanent human presence on the Moon, Mars, and beyond.",
+        "Supported NASA’s Space Biology program in examining the impacts of spaceflight on biological systems to enable a permanent human presence on the Moon, Mars, and beyond.",
         "Created a graphical user interface in Python for monitoring the accelerations felt by a biospecimen when rotating in a ground-based microgravity simulator.",
       ],
     },
