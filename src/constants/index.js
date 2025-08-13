476b58117445e31e9cdd28211226a65a0a56f618
Edwardrom25 <edwardromero781@gmail.com>
2025-08-13 10:14:55 -0500

    en dashes

--- src/constants/index.js
+++ src/constants/index.js
@@ -148,7 +148,7 @@ import {
       company_name: "SpaceX",
       icon: spacex,
       iconBg: "#000000",
-      date: "May 2025 - August 2025",
+      date: "May 2025 \u2013 August 2025",
       points: [
         "Supported the Facilities Infrastructure Engineering department in designing and operating critical electric infrastructure for SpaceX’s east coast launch facilities.",
         "Created a graphical user interface in LabVIEW to enable Falcon Recovery Controllers to monitor the Just Read the Instructions droneship’s onboard electric power systems.",
@@ -159,7 +159,7 @@ import {
       company_name: "NASA Kennedy Space Center",
       icon: nasawork,
       iconBg: "#FFFFFF",
-      date: "January 2025 - May 2025",
+      date: "January 2025 \u2013 May 2025",
       points: [
         "Supported the Microgravity Simulation Support Facility in examining the effects of spaceflight on biological systems to enable a sustained human presence on the Moon, Mars, and beyond.",
         "Created a graphical user interface in Python to enable Project Scientists to monitor the G-forces experienced by a biospecimen in a ground-based microgravity simulator.",
@@ -170,7 +170,7 @@ import {
       company_name: "Milhouse Engineering and Construction, Inc.",
       icon: milhouse,
       iconBg: "#FFFFFF",
-      date: "September 2024 - December 2024",
+      date: "September 2024 \u2013 December 2024",
       points: [
         "Supported ComEd’s transition to sustainable energy under the Future Energy Jobs Act, which requires at least 4,300 MW of new solar and wind power to be built in Illinois by 2030.",
         "Created construction drawings in AutoCAD and performed loading calculations in IKE PoleForeman to facilitate the interconnection of 35 solar farms to ComEd's electric grid.",
